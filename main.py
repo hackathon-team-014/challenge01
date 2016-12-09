590b18cd7bcd9998d8aa0d579b985b9e5ccf57ef
U-DIR\IMonteiro <IMonteiro@SLB-673DPX1.DIR.slb.com>
2016-12-08 23:39:06 -0600

    Fix on pubsub publish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask import jsonify
 from google.cloud import pubsub
+from flask.globals import current_app
 
 import logging
 import json
@@ -23,47 +24,52 @@ def status():
         
 @app.route('/api/capitals/<id>/publish', methods=['POST'])
 def publish(id):
-    received_json = request.get_json()
-    my_dump = json.dumps(received_json)
-    print my_dump
+    try:
+        received_json = request.get_json()
+        my_dump = json.dumps(received_json)
+        print my_dump
 
-    # projects/the-depot/topic/hack-it 
-    print received_json['topic'].split('/')[1]
-    pubsub_client = pubsub.Client(project=received_json['topic'].split('/')[1])
+        # projects/the-depot/topic/hack-it 
+        print received_json['topic'].split('/')[1]
+        pubsub_client = pubsub.Client(project=received_json['topic'].split('/')[1])
 
-    print 'got a pubsub client!!!', received_json['topic'].split('/')[3] 
-    topic_name = received_json['topic'].split('/')[3]
-    print topic_name
-    topic = pubsub_client.topic(topic_name)
-    print 'get topic from client!!!!'
+        print 'got a pubsub client!!!', received_json['topic'].split('/')[3] 
+        topic_name = received_json['topic'].split('/')[3]
+        print topic_name
+        topic = pubsub_client.topic(topic_name)
+        print 'get topic from client!!!!'
 
-    #topic_name_list = list()
-    #for t in pubsub_client.list_topics():
-        #topic_name_list.append(t.name)
+        #topic_name_list = list()
+        #for t in pubsub_client.list_topics():
+            #topic_name_list.append(t.name)
 
-    #if topic_name not in topic_name_list:
-        #topic.create()
+        #if topic_name not in topic_name_list:
+            #topic.create()
 
-    # print 'Creating datastore facade'
-    # gds = google_datastore.GoogleDataStore()
-    # print 'fetching...'
-    # result = gds.fetch(id)
-    # print 'got the result:'
-    # print result
-    result = '{}'
+        print 'Creating datastore facade'
+        gds = google_datastore.GoogleDataStore()
+        print 'fetching...'
+        result = gds.fetch(id)
+        print 'got the result:'
+        print result
 
-    if result == None:
-        return jsonify(code=404, message='Capital record not found'), 404
+        if result == None:
+            return jsonify(code=404, message='Capital record not found'), 404
 
-    print json.dumps(result)
-    data = json.dumps(result)
-    data = data.encode('utf-8')
+        print json.dumps(result)
+        data = json.dumps(result)
+        data = data.encode('utf-8')
 
-    message_id = topic.publish(data)
+        print 'Publishing on topic {}...'.format(topic_name)
+        message_id = topic.publish(data)
+        print 'Published on topic {}'.format(topic_name)
 
-    print('Message {} published.'.format(message_id))
-    return jsonify(messageId=message_id), 'Successfully published to topic', 200
+        print('Message {} published.'.format(message_id))
+        return current_app.response_class(('{ "messageId": ' + str(message_id) + ' }', '\n'), mimetype=current_app.config['JSONIFY_MIMETYPE']), 200
 
+    except Exception as e:
+        print e
+        return jsonify(code=500, message='Unexpected error'), 500
 
 @app.route('/api/capitals/<id>/store', methods=['POST'])
 def store(id):
